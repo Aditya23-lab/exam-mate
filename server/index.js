bef3553d9a86d915038124b632f681d2f85309ca
Aditya23_lab <cricketian55@gmail.com>
2025-09-27 17:58:03 +0530

    Fix CORS: allow requests from Vercel frontend

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["https://exam-mate-five.vercel.app", "http://localhost:5173"],
+    origin: ["https://exam-mate-3ty9.vercel.app", "http://localhost:5173"],
     credentials: true, // if you use cookies
   })
 );
